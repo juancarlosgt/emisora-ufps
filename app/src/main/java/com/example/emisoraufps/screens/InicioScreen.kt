6477aa153dbe404b397147dbbceca22c7a5ec29d
juancarlosgt <juancarlosgonzaleztorres4@gmail.com>
2025-10-20 20:45:41 -0500

    App Icon and Banner

--- app/src/main/java/com/example/emisoraufps/screens/InicioScreen.kt
+++ app/src/main/java/com/example/emisoraufps/screens/InicioScreen.kt
@@ -1,12 +1,43 @@
 package com.example.emisoraufps.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Card
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.emisoraufps.R
 
 @Composable
 fun InicioScreen() {
-    Text("Inicio")
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        // Banner
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color(0xFFAA1916)) 
+                .padding(10.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.ufps_radio_blanco),
+                contentDescription = "UFPS Radio",
+                modifier = Modifier
+                    .height(140.dp)
+                    .align(Alignment.CenterStart)
+            )
+        }
+
+
+    }
 }
 
 @Preview
